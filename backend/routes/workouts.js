0b41ecce6b84053da57b3830dc00085303df7e04
Feh Matos <felipempri@hotmail.com>
2025-08-31 08:09:05 -0300

    First commit

---
+++ backend/routes/workouts.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const {
+  createWorkout,
+  getWorkout,
+  getWorkouts,
+  deleteWorkout,
+  updateWorkouts,
+} = require("../controllers/workoutController");
+const requireAuth = require("../middleware/requireAuth");
+
+const router = express.Router();
+
+// require auth for all workout routes
+router.use(requireAuth);
+
+// GET all workouts
+router.get("/", getWorkouts);
+
+// GET single workout
+router.get("/:id", getWorkout);
+
+// POST new workout
+router.post("/", createWorkout);
+
+// DELETE workout
+router.delete("/:id", deleteWorkout);
+
+// UPDATE workout
+router.patch("/:id", updateWorkouts);
+
+module.exports = router;
